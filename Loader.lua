4e72092cb2079fbc1ed0a1e8f5d402f0cff3f683
DeFiqi <104684444+DeFiqi@users.noreply.github.com>
2026-06-07 12:42:59 +0700

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,13 +5,21 @@ local SupportedGames = {
     [6701277882] = "FishIt"
 }
 
-local BASE_URL = "https://raw.githubusercontent.com/USERNAME/FAWWA-FISH-IT/main"
+local BASE_URL = "https://raw.githubusercontent.com/DeFiqi/FAWWA-FISH-IT/main"
 
 local GameName = SupportedGames[game.GameId]
 
 if not GameName then
-    warn(HUB_NAME .. " | Game not supported")
+    warn("[" .. HUB_NAME .. "] Game not supported!")
     return
 end
 
-loadstring(game:HttpGet(BASE_URL .. "/Games/" .. GameName .. ".lua"))()
+local ScriptURL = BASE_URL .. "/Games/" .. GameName .. ".lua"
+
+local Success, Error = pcall(function()
+    loadstring(game:HttpGet(ScriptURL))()
+end)
+
+if not Success then
+    warn("[" .. HUB_NAME .. "] Error: " .. tostring(Error))
+end
